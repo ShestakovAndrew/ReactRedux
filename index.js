094f6e08d38900c6be0f5d37393d61af50485282
Andrew <globalshow6@gmail.com>
2023-05-30 17:25:19 +0300

    Redux Toolkit Tutorials: 12 - Combine Reducers

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const redux = require('redux')
 const createStore = redux.createStore
 const bindActionCreators = redux.bindActionCreators
+const combineReducers = redux.combineReducers
 
 const CAKE_ORDERED = 'CAKE_ORDERED'
 const CAKE_RESTOCKED = 'CAKE_RESTOCKED'
@@ -73,19 +74,24 @@ const iceCreamReducer = (state = initialIceCreamState, action) => {
 		case ICE_CREAM_ORDERED:
 			return {
 				...state,
-				numOnIceCreams: state.numOnIceCreams - 1,
+				numOfIceCreams: state.numOfIceCreams - 1,
 			}
 		case ICE_CREAM_RESTOCKED:
 			return {
 				...state,
-				numOnIceCreams: state.numOnIceCreams + action.payload,
+				numOfIceCreams: state.numOfIceCreams + action.payload,
 			}
 		default:
 			return state
 	}
 }
 
-const store = createStore(reducer)
+const rootReducer = combineReducers({
+	cake: cakeReducer,
+	iceCream: iceCreamReducer
+})
+
+const store = createStore(rootReducer)
 console.log('Initial state', store.getState())
 
 const unsubscribe = store.subscribe(() => console.log('update state ', store.getState()))
