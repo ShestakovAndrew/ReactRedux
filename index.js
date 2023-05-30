9032a0db8bf57e083ba24e1fef44ccb610c3546c
Andrew <globalshow6@gmail.com>
2023-05-30 17:00:23 +0300

    Redux Toolkit Tutorials: 8 - Restocking Cakes

--- index.js
+++ index.js
@@ -2,11 +2,19 @@ const redux = require('redux')
 const createStore = redux.createStore
 
 const CAKE_ORDERED = 'CAKE_ORDERED'
+const CAKE_RESTOCKED = 'CAKE_RESTOCKED'
 
 function orderCake() {
 	return {
 		type: CAKE_ORDERED,
-		quantity: 1,
+		payload: 1,
+	}
+}
+
+function restockCake(qty = 1) {
+	return {
+		type: CAKE_RESTOCKED,
+		payload: qty,
 	}
 }
 
@@ -19,7 +27,12 @@ const reducer = (state = initialState, action) => {
 		case CAKE_ORDERED:
 			return {
 				...state,
-				numOfCakes: state.numOfCakes - 1
+				numOfCakes: state.numOfCakes - 1,
+			}
+		case CAKE_RESTOCKED:
+			return {
+				...state,
+				numOfCakes: state.numOfCakes + action.payload,
 			}
 		default:
 			return state
@@ -34,5 +47,8 @@ const unsubscribe = store.subscribe(() => console.log('update state ', store.get
 store.dispatch(orderCake())
 store.dispatch(orderCake())
 store.dispatch(orderCake())
+
+store.dispatch(restockCake(3))
+
 
 unsubscribe()
